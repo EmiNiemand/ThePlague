fd147da9f69d710e20bdadff04fb09aa3982cd0a
Michalina Śledzikowska <236669@edu.p.lodz.pl>
2022-11-05 13:25:49 +0100

    Customize inspector appearance

--- Assets/Scripts/GamepadHaptics.cs
+++ Assets/Scripts/GamepadHaptics.cs
@@ -3,18 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[System.Serializable]
+public struct RumbleSettings
+{
+    [Range(0.0f, 1.0f)]
+    public float intensity;
+    [Range(0.0f, 5.0f)]
+    public float duration;
+}
+
 public class GamepadHaptics : MonoBehaviour
 {
-
-    public float attackRumbleIntensity;
-    public float attackRumbleDuration;
+    public RumbleSettings attackSettings;
 
     public IEnumerator Attack()
     {
         if(!gamepadConnected()) yield break;
 
-		Gamepad.current.SetMotorSpeeds(attackRumbleIntensity, attackRumbleIntensity);
-        yield return new WaitForSeconds(attackRumbleDuration);
+		Gamepad.current.SetMotorSpeeds(attackSettings.intensity, attackSettings.intensity);
+        yield return new WaitForSeconds(attackSettings.duration);
 		Gamepad.current.SetMotorSpeeds(0, 0);
     }
 
