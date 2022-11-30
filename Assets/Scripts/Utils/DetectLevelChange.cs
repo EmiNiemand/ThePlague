965aefe54444cf4e400994dd8542b33a5abf2b1d
EmiNiemand <Szymon_PL@live.com>
2022-11-30 13:25:03 +0100

    fix changing scene

--- Assets/Scripts/Utils/DetectLevelChange.cs
+++ Assets/Scripts/Utils/DetectLevelChange.cs
@@ -6,15 +6,7 @@ public class DetectLevelChange : MonoBehaviour
     private Scene _level;
     
     [SerializeField] private string levelChange;
-    [SerializeField] private GameObject player;
-    [SerializeField] private GameObject camera;
-    
-    private void Awake()
-    {
-        DontDestroyOnLoad(player);
-        DontDestroyOnLoad(camera);
-    }
-    
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
