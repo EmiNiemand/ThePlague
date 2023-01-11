734a06f3fc378c49a567eef8d6e9e9038008d29f
Tomasz Lesiak <tomek.l.lesiak@gmail.com>
2023-01-11 12:02:51 +0100

    dash

--- Assets/Scripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerCombat.cs
@@ -24,6 +24,7 @@ public class PlayerCombat : MonoBehaviour
 
     private bool canAttack = true;
 
+
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,12 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Numerics;
+using System.Security.Cryptography;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using Vector2 = UnityEngine.Vector2;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float dashSpeed;
+    [SerializeField] private float dashDuration;
+
+    private float dashTimer;
+    private bool dashing;
+
     private Vector2 MoveAxis;
     Rigidbody2D Rigidbody;
     public bool isUsePressed = false;
@@ -14,6 +23,8 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float MoveSpeed;
 
     private PlayerCombat playerCombat;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,13 +33,35 @@ public class PlayerMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     void FixedUpdate()
     {
+        if (dashTimer > 0)
+        {
+            dashTimer -= Time.deltaTime;
+            RaycastHit2D[] hits = Physics2D.BoxCastAll(transform.position, Vector2.one, transform.rotation.z, MoveAxis, 1.5f);
+            bool canMove = true;
+
+            foreach (var hit in hits)
+            {
+                if (hit.collider.gameObject.CompareTag("Environment") || hit.collider.gameObject.CompareTag("Obstacles") || hit.collider.gameObject.CompareTag("NPC"))
+                {
+                    canMove = false; break;
+                }
+            }
+            if (!canMove)
+            {
+                GetComponent<PolygonCollider2D>().enabled = true;
+                Rigidbody.AddForce(MoveAxis * dashSpeed, ForceMode2D.Impulse);
+                return;
+            }
+            GetComponent<PolygonCollider2D>().enabled = false;
+            Rigidbody.AddForce(MoveAxis * dashSpeed, ForceMode2D.Impulse); 
+        }
+        else
+        {
+            GetComponent<PolygonCollider2D>().enabled = true;
+            dashing = false;
+        }
         Rigidbody.AddForce(MoveAxis * (MoveSpeed + playerCombat.GetAdditionalMoveSpeed()), ForceMode2D.Impulse);
     }
 
@@ -41,11 +74,32 @@ public class PlayerMovement : MonoBehaviour
     {
         if (context.started)
         {
-            isUsePressed = true;
+                isUsePressed = true;
         }
         else if (context.canceled)
         {
             isUsePressed = false;
         }
     }
+
+    public void OnDash(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            {
+                if (!dashing)
+                {
+                    Dash();
+                }
+            }
+        }
+    }
+
+    private void Dash()
+    {
+        dashTimer = dashDuration;
+        dashing = true;
+    }
+
+
 }
